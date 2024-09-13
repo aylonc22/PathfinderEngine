5ec75ded94f0ad3c700aa4784fcc659c38d2746c
Aylon <aylonc1@gmail.com>
2024-09-13 17:38:26 +0300

    first commit

---
+++ src/main/java/PathfinderEngine/example/PathfinderEngine/PathfinderEngineApplication.java
@@ -0,0 +1,13 @@
+package PathfinderEngine.example.PathfinderEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathfinderEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathfinderEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PathfinderEngine/example/PathfinderEngine/PathfinderEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package PathfinderEngine.example.PathfinderEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathfinderEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
