37d14232d67ce06f690c3daac2145caece985d5a
Aylon <aylonc1@gmail.com>
2024-09-16 17:42:28 +0300

    built get node by id method and couple of changes to the graph params

--- src/main/java/PathfinderEngine/example/PathfinderEngine/beans/graph/Graph.java
+++ src/main/java/PathfinderEngine/example/PathfinderEngine/beans/graph/Graph.java
@@ -1,26 +1,30 @@
 package PathfinderEngine.example.PathfinderEngine.beans.graph;
 
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Component
 public class Graph {
+    private final Map<String, Node> nodeMap = new HashMap<>();
     private final Map<Node, List<Edge>> adjList = new HashMap<>();
 
-    public void addNode(Node node){
-        adjList.putIfAbsent(node,new ArrayList<>());
+    public void addNode(Node node) {
+        nodeMap.putIfAbsent(node.getId(), node);
+        adjList.putIfAbsent(node, new ArrayList<>());
     }
 
-    public void addEdge(Node from,Node to,int weight){
+    public Node getNodeById(String id) {
+        return nodeMap.get(id);
+    }
+
+    public void addEdge(Node from, Node to, int weight) {
         adjList.computeIfAbsent(from, k -> new ArrayList<>()).add(new Edge(from, to, weight));
         adjList.computeIfAbsent(to, k -> new ArrayList<>()).add(new Edge(to, from, weight)); // For undirected graph
-    }
-
-    public Map<Node, List<Edge>> getAdjList() {
-        return adjList;
-    }
-
-    public Node getNodeById(String fromId) {
     }
 }
